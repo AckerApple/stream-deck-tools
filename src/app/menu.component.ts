209eea488d558fb05f220dacf6a201b82b01474b
Acker Apple <y>
2022-12-08 21:56:14 -0500

    gh-pages

--- src/app/menu.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core'
-import { fixProfilesNav } from './app.routing.module'
-
-@Component({
-  templateUrl: './menu.component.html',
-})
-export class MenuComponent {
-  menu = [ fixProfilesNav ]
-}
